aa99160dd0cc8a657b9343181bbc9e2f924e6db0
Mesum Raza Hemani <mes.javacca@gmail.com>
2021-02-06 16:21:28 +0500

    Update qa_quran.py

--- qa_quran.py
+++ qa_quran.py
@@ -3,13 +3,9 @@ from haystack.preprocessor.utils import convert_files_to_dicts
 from haystack.reader.farm import FARMReader
 from haystack.document_store.memory import InMemoryDocumentStore
 from haystack.retriever.sparse import TfidfRetriever
-import streamlit as st
 from os import path
-import pandas as pd
+import gradio
 
-st.markdown("<center> <h1> 📜 Questions And Answering Using Quran's English Translation </h1> </center>", True)
-
-@st.cache
 def read_corpus():
     document_store = InMemoryDocumentStore()
     doc_dir = "Quran"
@@ -17,36 +13,30 @@ def read_corpus():
     document_store.write_documents(dicts)
     return document_store
 
-@st.cache(allow_output_mutation=True)
 def retriever():
     document_store = read_corpus()
     retriever = TfidfRetriever(document_store=document_store)
     return retriever
 
-question = st.text_input('Input your question here:')
+retriever = retriever()
 
-if st.button('Ask'):
-    with st.spinner('Reading all the translations from all over Quran'):
-        retriever = retriever()
-        
-        if not(path.exists('data/mlm-temp')):
-            reader = FARMReader(model_name_or_path="deepset/minilm-uncased-squad2", use_gpu=False)
-            reader.save(directory='data/mlm-temp')
-            st.info('Downloaded Fresh Model')
-        else:
-            reader = FARMReader(model_name_or_path="data/mlm-temp", use_gpu=False)
-            st.info('Re-Used Model')
-            
-        finder = Finder(reader, retriever)
-        
-        prediction = finder.get_answers(question=question, top_k_retriever=10, top_k_reader=5)
+if not(path.exists('data/mlm-temp')):
+    reader = FARMReader(model_name_or_path="deepset/minilm-uncased-squad2", use_gpu=False)
+    reader.save(directory='data/mlm-temp')
+else:
+    reader = FARMReader(model_name_or_path="data/mlm-temp", use_gpu=False)
+    
+finder = Finder(reader, retriever)
 
-        keys=['answer','context','meta','probability','score']
-        print(list( map(prediction.get, ['query'])))
-        print("\n")
-        answer_frame=pd.DataFrame.from_records([list( map(i.get, keys)) for i in prediction['answers']])
-        answer_frame.columns=['answer','reference','Surah','confidence','score']
-        answer_frame['Surah']=answer_frame['Surah']
-        st.table(answer_frame.T)
-        
-        del reader,prediction,finder
+def ask_question(inp):
+    prediction = finder.get_answers(question=inp, top_k_retriever=10, top_k_reader=5)
+    return prediction
+
+gradio.Interface(
+    fn=ask_question
+    ,inputs="textbox"
+    , outputs="json"
+    ,layout="vertical"
+    ,title="ASK A QUESTION FROM HOLY QURAN IN ENGLISH"
+    ,examples=[['Who is Muhammad?'],['Who are Gog and Magog?'],['Who is Satan?']]
+).launch()
